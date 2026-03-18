c67be5f2a23a46a55148ae85a641fd388697d191
Priyanshu Singh <priyanshu2002krsingh@gmail.com>
2026-03-18 11:51:17 +0530

    auth controllers

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,5 +1,8 @@
 import userModel from "../models/user.model.js";
 import crypto from "crypto";
+import jwt from "jsonwebtoken";
+import config from "../config/config.js";
+
 
 export async function register(req, res) {
     const { username, email, password } = req.body;
@@ -23,5 +26,21 @@ export async function register(req, res) {
         password : hashedPassword
     })
 
+    const token = jwt.sign({
+        id: user._id
+    }, config.JWT_SECRET,
+    {
+        expiresIn: "1d"
+    }
+)
+
+res.status(201).json({
+    message: "User registered successfully",
+    user: {
+        username: user.username,
+        email: user.email,
+    },
+    token
+})
 
 }
